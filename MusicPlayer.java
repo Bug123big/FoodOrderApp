a7e2fa76b4aa78fd88d628c51e80201abb7ca412
diko6666 <chenghanh777@gmail.com>
2026-05-19 16:08:51 -0400

    Refactor music loading to use resource URL
    Updated music file handling to use resource URL instead of file path.

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -1,29 +1,30 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
-import java.io.File;
+import java.net.URL;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage stage) {
+        URL musicUrl = Main.class.getResource("/Music.mp3");
 
-        String path = "music/song.mp3";
+        if (musicUrl == null) {
+            throw new IllegalStateException("Music.mp3 was not found in the program resources.");
+        }
 
-        Media media = new Media(new File(path).toURI().toString());
+        Media media = new Media(musicUrl.toExternalForm());
         MediaPlayer player = new MediaPlayer(media);
 
-        // loop forever
         player.setCycleCount(MediaPlayer.INDEFINITE);
-
-        // volume (0.0 to 1.0)
-        player.setVolume(0.5);
-
-        // autoplay
+        player.setVolume(0.3);
         player.play();
 
+        stage.setTitle("Music Player");
+        stage.setScene(new Scene(new Label("Playing Music.mp3"), 260, 100));
         stage.show();
     }
 
